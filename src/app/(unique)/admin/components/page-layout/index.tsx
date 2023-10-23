636cf0d322598d2da2b58ced9a8959b455ec1b7b
kuro-xx <k.kuroda@miwaza.co.jp>
2023-10-23 18:43:22 +0900

    fix: 管理画面とユーザー画面のレイアウトが一緒になるバグを修正

--- src/app/(unique)/admin/components/page-layout/index.tsx
+++ src/app/(unique)/admin/components/page-layout/index.tsx
@@ -8,7 +8,7 @@ export const MyPageLayout = ({ children }: Props) => {
   return (
     <div id="page">
       <header id="masthead" className={styles.header}>
-        <div className={styles.logo}>My Blog</div>
+        <div className={styles.logo}>My Blog 管理画面</div>
       </header>
       <main id="main" className={styles.main}>
         {children}
