3c2a10bf1973e38e6b1dc877adefa9085dfb5c94
kuro-xx <k.kuroda@miwaza.co.jp>
2023-11-06 11:56:32 +0900

    feature: 静的データベースから記事の一覧を取得

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import { articlesDatabase } from './dummy-database/articles';
 import { applyServerSettings } from './settings';
 
 const app = express();
@@ -9,6 +10,34 @@ const port = 8000;
 applyServerSettings(app);
 
 // ↓↓↓ バックエンド処理を記述して実際に開発してみましょう！！
+
+// APIのURL http://localhost:8000/admin/articles
+// 作成が完了したら http://localhost:3000/admin にアクセスして確認してみましょう！
+app.get('/admin/articles', async (req, res) => {
+  const articles = articlesDatabase.map((record) => {
+    return {
+      id: record.id,
+      title: record.title,
+      content: record.content,
+      category: record.category,
+      status: record.status,
+      createdAt: formatDateInJa(record.createdAt),
+      updatedAt: formatDateInJa(record.updatedAt),
+    };
+  });
+
+  res.json({ data: articles });
+});
+
+const formatDateInJa = (date: Date) => {
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  return `${year}/${month}/${day} ${hours}:${minutes}`;
+};
 
 // ↑↑↑ バックエンド処理を記述して実際に開発してみましょう！！
 
