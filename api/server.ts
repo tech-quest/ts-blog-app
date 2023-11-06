31995ada006695ae01f55e8646cdff1bc9584e47
kuro-xx <k.kuroda@miwaza.co.jp>
2023-11-06 14:19:10 +0900

    feature: Prisma でデータベースに記事を編集

--- api/server.ts
+++ api/server.ts
@@ -108,6 +108,48 @@ app.post('/admin/articles/create', async (req, res) => {
   res.json({ data: { id: record.id.toString(10) } });
 });
 
+// APIのURL http://localhost:8000/admin/articles/update
+// 作成が完了したら http://localhost:3000/admin/update/1 にアクセスして確認してみましょう！
+app.post('/admin/articles/update', async (req, res) => {
+  const { articleId, title, content, category, status } = req.body;
+
+  const id = Number(articleId);
+  if (Number.isNaN(id)) {
+    res.status(400).json({ error: { message: 'ID 形式が不正な形式となっています' } });
+    return;
+  }
+
+  const requiredMessage = '未入力の内容があります';
+
+  if (title === '') {
+    res.status(400).json({ error: { message: requiredMessage } });
+    return;
+  }
+
+  if (content === '') {
+    res.status(400).json({ error: { message: requiredMessage } });
+    return;
+  }
+
+  if (category === '') {
+    res.status(400).json({ error: { message: requiredMessage } });
+    return;
+  }
+
+  if (status === '') {
+    res.status(400).json({ error: { message: requiredMessage } });
+    return;
+  }
+
+  try {
+    const record = await prisma.article.update({ where: { id }, data: { title, content, category, status } });
+
+    res.json({ data: { id: record.id.toString(10) } });
+  } catch {
+    res.status(500).json({ error: { message: 'データベース操作に失敗しました。' } });
+  }
+});
+
 const formatDateInJa = (date: Date) => {
   const year = date.getFullYear().toString();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
